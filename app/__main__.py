7fd0366d324b8e4af5ff0df4a0c88c8aaa1e5df9
blipp <6Fqshfzlx3hpKgZEWZbu>
2021-04-27 14:44:05 -0400

    starting adding python model, not fully done

--- app/__main__.py
+++ app/__main__.py
@@ -2,6 +2,7 @@ from delta.tables import *
 import yfinance as yf
 import time
 import dbutils
+import uuid
 
 
 def main():
@@ -10,11 +11,11 @@ def main():
     pdf = yf.download(tickers='UBER', period='10y', interval='1d')
     pdf["AdjClose"] = pdf["Adj Close"]
     pdf = pdf.drop("Adj Close", axis=1)
-    uuid = str(uuid.uuid1()).replace('-', '')
+    id = str(uuid.uuid1()).replace('-', '')
     timestamp = int(time.time())
     dbutils.fs.mkdirs("/dbfs/datalake")
     df = spark.createDataFrame(pdf)
-    df.write.format("delta").save(f"/dbfs/datalake/stocks_{uuid}_{timestamp}/data")
+    df.write.format("delta").save(f"/dbfs/datalake/stocks_{id}_{timestamp}/data")
 
 
 if __name__ == "__main__":
